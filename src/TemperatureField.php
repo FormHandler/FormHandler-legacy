e12926dc129ede3ed0d9bb324998c883f8a3fe92
ruben <ruben@color-base.com>
2016-02-02 12:33:11 +0100

    - Reverted changes

--- src/TemperatureField.php
+++ src/TemperatureField.php
@@ -24,31 +24,6 @@
  */
 class TemperatureField extends \FormHandler\Field\Temperature
 {
-    private $checkbox;
-
-    public function __construct(\FormHandler $form, $name)
-    {
-        parent::__construct($form, $name);
-
-        $this->checkbox = new CheckBox($this->form_object, $this->name .'_empty');
-        $this->checkbox->setOptions(array(1 => '<em>Not applicable</em>'));
-
-        $form->_setJS("$('#". $name ."_empty_1').on('change',function()\n
-            {\n
-             var state = !!$(this).prop('checked');\n
-             $('#". $name ."_temperature, #". $name ."_unit').prop('disabled',state);\n
-            });"
-        ,false,false);
-    }
-
-    public function _getViewValue()
-    {
-        return (is_null($this->getValue()))
-            ? '<em>Not applicable</em>'
-            : parent::_getViewValue();
-
-    }
-
     public static function set(\FormHandler\FormHandler $form, $title, $name, $validator = null)
     {
         $field = parent::set($form, $title, $name);
@@ -73,67 +48,5 @@ class TemperatureField extends \FormHandler\Field\Temperature
         }
 
         return parent::setValidator(FormHandler::parseValidator($validator, $this));
-    }
-
-    public function setValue($value, $forced = false)
-    {
-        if(is_null($value))
-        {
-            $this->checkbox->setValue(true);
-        }
-        return parent::setValue($value, $forced);
-    }
-
-    /**
-     * Return fields
-     *
-     * @return type
-     */
-    public function getField()
-    {
-        // view mode enabled ?
-        if($this->getViewMode())
-        {
-            // get the view value..
-            return $this->_getViewValue();
-        }
-
-        return parent::getField() .'<br>'. $this->checkbox->getField();
-    }
-
-    /**
-     * Set disabled
-     *
-     * @param boolean $bool
-     * @return Field
-     */
-    public function setDisabled($bool = true)
-    {
-        $this->checkbox->setDisabled($bool);
-        return parent::setDisabled($bool);
-    }
-
-    public function getDisabled()
-    {
-        $empty_value = $this->checkbox->getValue();
-
-        return (!empty($empty_value))
-            ? true
-            : parent::getDisabled();
-    }
-
-    /**
-     * Get if the field is in error state
-     *
-     * @return boolean
-     * @author Ruben de Vos
-     */
-    public function getErrorState()
-    {
-        $empty_value = $this->checkbox->getValue();
-
-        return (!empty($empty_value))
-            ? false
-            : parent::getErrorState();
     }
 }
